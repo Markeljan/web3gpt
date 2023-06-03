f2198910aac4e960880bb14ba29a38b0cc47f4ff
Markeljan Sokoli <Markeljan19@gmail.com>
2023-06-03 16:41:13 -0400

    update vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { GatewayProvider } from "@civic/ethereum-gateway-react";
 import { ethers } from "ethers";
 import App from "./app";
@@ -12,27 +12,37 @@ declare global {
 }
 
 const Home = () => {
-  const [signer, setSigner] = useState(null);
-  const openWallet = async () => {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
+  const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner | null>(null);
+
+  const openWallet = useCallback(async () => {
+    if (window?.ethereum) {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (err) {
+        console.error('Failed to request accounts:', err);
+      }
+    }
+  }, []);
 
   useEffect(() => {
-    if (typeof window.ethereum !== "undefined") {
+    if (typeof window?.ethereum !== "undefined") {
       openWallet();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      setSigner(signer as any);
-      //eth get accounts to open metamask
+      try {
+        const signer = provider.getSigner();
+        setSigner(signer);
+      } catch (err) {
+        console.error('Failed to get signer:', err);
+      }
     } else {
       console.log('Please install MetaMask or XDC Pay and switch to the XDC Apothem Network.');
     }
-  }, [window?.ethereum]);
+  }, [openWallet]);
 
   return (
     signer ? (
       <GatewayProvider
-        wallet={signer}
+        wallet={signer as any}
         gatekeeperNetwork="ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6">
           <App />
       </GatewayProvider>
