e8b58143e22b1083f2a0aad6e778188915c61ea0
Markeljan Sokoli <Markeljan19@gmail.com>
2023-06-19 10:40:37 -0400

    update headers

--- app/utils/createResponse.ts
+++ app/utils/createResponse.ts
@@ -4,10 +4,6 @@ const createResponse = (status: number, data?: any, headers?: HeadersInit) => {
   const payload = data ? { data } : {};
 
   const defaultHeaders = {
-    "Access-Control-Allow-Origin": "https://chat.openai.com",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, openai-ephemeral-user-id, openai-conversation-id",
   };
 
   return NextResponse.json(payload, {
