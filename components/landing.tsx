7d3a6c620954d509901adbc8c85d0ef651838f9c
Alex Biet <alexandrubiet@gmail.com>
2023-09-02 01:19:21 +0200

    Landing page chat - wip

--- components/landing.tsx
+++ components/landing.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 
 export function Landing({}) {
   return (
-    <div className="mx-auto max-w-2xl px-4 text-center">
+    <>
+    <div className="mx-auto max-w-2xl px-4 text-center border border-gray-600 rounded-2xl mb-16">
+
       <div className="relative mb-16 mt-24 flex h-8 w-full">
         <Image
           src="/w3gpt-logo.svg"
@@ -16,6 +18,46 @@ export function Landing({}) {
       <p className="scroll-m-20 text-2xl tracking-tight">
         Deploy smart contracts with AI
       </p>
+
+      <div className="flex flex-row">
+        <div className="basis-1/3">
+          <img src="logo.png" alt="logo" />
+          <h3 className="">Write Smart Contracts</h3>
+          <p className="">Generate custom smart contracts using a prompt.</p>
+        </div>
+        <div className="basis-1/3">
+          <img src="logo.png" alt="logo" />
+          <h3 className="">Deploy to Blockchains</h3>
+          <p className="">Generate custom smart contracts using a prompt.</p>
+        </div>
+        <div className="basis-1/3">
+          <img src="logo.png" alt="logo" />
+          <h3 className="">Faster Development</h3>
+          <p className="">Speed up development and skip the long and complex setups.</p>
+        </div>
+      </div>
+
     </div>
+
+
+    <div className="mx-auto max-w-2xl px-4 text-center border border-gray-600 rounded-2xl mb-16">
+
+      <div className="relative mb-16 mt-24 flex h-8 w-full">
+      </div>
+      <p className="scroll-m-20 text-2xl tracking-tight">
+        Weekly Updates
+      </p>
+
+      <p>Sign up for development updates and early access to latest features</p>
+
+      <input type="text" placeholder="Your email"/> <button type="submit">Send</button>
+
+      <p>No spam, we promise :)</p>
+
+    </div>
+
+
+
+    </>
   )
 }
