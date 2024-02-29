cba381ee4fc11f0bf995e3c55fe5dd9b0528f23c
Markeljan Sokoli <Markeljan19@gmail.com>
2024-02-29 00:38:04 -0700

    lite

--- next.config.js
+++ next.config.js
@@ -8,6 +8,27 @@ const nextConfig = {
       }
     ]
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/deploy-contract*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'POST'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
