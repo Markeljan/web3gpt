b193ee6b2f4c2b73196c414152e7e6d2a7771c9d
Markeljan Sokoli <Markeljan19@gmail.com>
2024-05-12 15:28:14 +1000

    yolo commit

--- next.config.js
+++ next.config.js
@@ -7,6 +7,27 @@ const nextConfig = {
         hostname: "avatars.githubusercontent.com"
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/external-contract",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, PUT, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization"
+          }
+        ]
+      }
+    ]
   }
 }
 
