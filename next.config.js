3868b667886c698ec94278aabe941de6e0ee06ac
Markeljan Sokoli <Markeljan19@gmail.com>
2024-05-12 18:06:46 +1000

    yolo commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com"
+      },
+      {
+        protocol: "https",
+        hostname: "**"
       }
     ]
   },
