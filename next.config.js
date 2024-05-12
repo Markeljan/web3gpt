94b9446086b7f48db8b2bc8af320c9b1ad8024a4
Markeljan Sokoli <Markeljan19@gmail.com>
2024-05-12 15:43:39 +1000

    yolo commit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/external-contract",
+        source: "/api/external-deploy",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
