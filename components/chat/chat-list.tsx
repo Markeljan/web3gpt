cb846b224e9f207445ee433e3e701a05a03b9300
Markeljan Sokoli <Markeljan19@gmail.com>
2024-05-28 16:18:21 -0400

    chat-w

--- components/chat/chat-list.tsx
+++ components/chat/chat-list.tsx
@@ -2,7 +2,6 @@ import type { AssistantStatus, Message } from "ai"
 
 import { ChatMessage } from "@/components/chat/chat-message"
 import { Separator } from "@/components/ui/separator"
-import { nanoid } from "@/lib/utils"
 
 export type ChatList = {
   messages: Message[]
@@ -16,9 +15,9 @@ export function ChatList({ messages, avatarUrl, status }: ChatList) {
   }
 
   return (
-    <div className="relative flex flex-col mx-auto max-md:max-w-2xl max-w-4xl w-full p-2 md:translate-x-[10%]">
+    <div className="relative flex flex-col mx-auto max-md:max-w-2xl max-w-3xl w-full p-2 md:translate-x-[10%]">
       {messages.map((message, index) => (
-        <div className="flex flex-col w-full" key={`${message.id}-${nanoid()}`}>
+        <div className="flex flex-col w-full" key={`${message.id}`}>
           <ChatMessage message={message} avatarUrl={avatarUrl} status={status} />
           {index < messages.length - 1 && <Separator className="my-4 md:my-8 md:-translate-x-[5%]" />}
         </div>
