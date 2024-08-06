95562610f1232aed4b28245d9330887a164796a4
Markeljan Sokoli <Markeljan19@gmail.com>
2024-08-05 21:38:40 -0400

    cors, CSP

--- next.config.js
+++ next.config.js
@@ -21,6 +21,27 @@ module.exports = {
   async headers() {
     return [
       {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://app.safe.global https://*.blockscout.com;"
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, content-type, Authorization"
+          }
+        ]
+      },
+      {
         source: "/api/external-deploy",
         headers: [
           {
@@ -36,7 +57,7 @@ module.exports = {
             value: "Content-Type, Authorization, web3gpt-api-key"
           }
         ]
-      },
+      }
     ]
   }
 }
