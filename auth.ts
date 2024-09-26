2c627676fb94845837f403386cbace274e98adba
Markeljan Sokoli <Markeljan19@gmail.com>
2024-09-26 05:06:00 -0400

    auth patch

--- auth.ts
+++ auth.ts
@@ -1,20 +1,12 @@
 import NextAuth, { type DefaultSession } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
 
 import { storeUser } from "@/lib/data/kv"
-import { createSiweMessage } from "viem/siwe"
 
 declare module "next-auth" {
-  interface User {
-    id: string
-    address: string
-  }
-
   interface Session extends DefaultSession {
     user: {
       id: string
-      address: string
     } & DefaultSession["user"]
   }
 }
@@ -25,45 +17,7 @@ export const {
   signIn,
   signOut
 } = NextAuth({
-  providers: [
-    GitHub,
-    CredentialsProvider({
-      name: "Ethereum",
-      credentials: {
-        message: {
-          label: "Message",
-          type: "text",
-          placeholder: "0x0"
-        },
-        signature: {
-          label: "Signature",
-          type: "text",
-          placeholder: "0x0"
-        }
-      }
-      // async authorize(credentials) {
-      //   try {
-      //     const siwe = createSiweMessage(JSON.parse(credentials?.message || "{}"))
-      //     const nextAuthUrl = new URL(process.env.AUTH_URL)
-
-      //     const result = await siwe.verify({
-      //       signature: credentials?.signature || "",
-      //       domain: nextAuthUrl.host,
-      //       nonce: await getCsrfToken({ req })
-      //     })
-
-      //     if (result.success) {
-      //       return {
-      //         id: siwe.address
-      //       }
-      //     }
-      //     return null
-      //   } catch (e) {
-      //     return null
-      //   }
-      // }
-    })
-  ],
+  providers: [GitHub],
   callbacks: {
     async jwt({ token, profile }) {
       if (profile?.id) {
@@ -79,7 +33,7 @@ export const {
       return token
     },
 
-    session({ session, token, user }) {
+    session({ session, token }) {
       if (token?.id) {
         session.user.id = String(token.id)
       }
@@ -87,8 +41,7 @@ export const {
         ...session,
         user: {
           ...session.user,
-          id: String(token.id),
-          address: user.address
+          id: String(token.id)
         }
       }
     }
