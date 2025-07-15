420d0907b60a281c84cbbb2d4dd92864cbd0e0db
Markeljan Sokoli <Markeljan19@gmail.com>
2025-07-14 22:44:50 -0400

    add dynamic config, dynamic import for sdk

--- app/api/farcaster/route.ts
+++ app/api/farcaster/route.ts
@@ -3,6 +3,21 @@ import { DEPLOYMENT_URL } from "@/lib/config"
 
 const DYNAMIC_URL = process.env.NODE_ENV === "production" ? DEPLOYMENT_URL : "https://markeljan.a.pinggy.link"
 
+const DYNAMIC_ACCOUNT_ASSOCIATION =
+  process.env.NODE_ENV === "production"
+    ? {
+        header:
+          "eyJmaWQiOjI4MTI2MCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDNiN0M0NzU1ZTFBNzdDMjBBRDI4N2I4Q2RGMDRkOTdiOTY3YjA3NEYifQ",
+        payload: "eyJkb21haW4iOiJ3M2dwdC5haSJ9",
+        signature: "yNtnGwqI5Z4QbFM3zGPIct7c8X7w3Uz9AfkZwrOFps0P+zm2Fq8ecGj0qWC8nty7UzCBOmQ4soEa/bHifAA5VRs=",
+      }
+    : {
+        header:
+          "eyJmaWQiOjI4MTI2MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDJiOTY1MGNBZTRDNUU2OTQ4RWNBRDYxQjNEYUI2YTk5OTc4NzFlOEQifQ",
+        payload: "eyJkb21haW4iOiJtYXJrZWxqYW4uYS5waW5nZ3kubGluayJ9",
+        signature:
+          "MHhhYzg1NDFiMWI3ZDM4OGY3OWU5NzA5MzY3MGIzNTAxYzUyZDVkYWVjMTIyYmYzNDYyMzU1ZmE2ODM2N2Q2YTgwMTVlNmI5ZmRiOGFjYmNmMDNiMmRjYmQyYzE1ZDE0ODc5MDc4MTA5YzAwYTg2OGJiOTBlZjhmZWNiODE5YmM3ZTFj",
+      }
 export async function GET() {
   const manifest = {
     miniapp: {
@@ -26,12 +41,7 @@ export async function GET() {
       noindex: false,
       canonicalDomain: new URL(DYNAMIC_URL).hostname,
     },
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjI4MTI2MCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDNiN0M0NzU1ZTFBNzdDMjBBRDI4N2I4Q2RGMDRkOTdiOTY3YjA3NEYifQ",
-      payload: "eyJkb21haW4iOiJ3M2dwdC5haSJ9",
-      signature: "yNtnGwqI5Z4QbFM3zGPIct7c8X7w3Uz9AfkZwrOFps0P+zm2Fq8ecGj0qWC8nty7UzCBOmQ4soEa/bHifAA5VRs=",
-    },
+    accountAssociation: DYNAMIC_ACCOUNT_ASSOCIATION,
   }
 
   return NextResponse.json(manifest, {
--- components/miniapp-initializer.ts
+++ components/miniapp-initializer.ts
@@ -1,14 +1,17 @@
 "use client"
 
-import { sdk } from "@farcaster/miniapp-sdk"
 import { useEffect } from "react"
 
 export const MiniAppInitializer = (): React.ReactNode => {
   useEffect(() => {
-    const initialize = async () => {
-      await sdk.actions.ready()
+    const url = new URL(window.location.href)
+    const isMini = url.pathname.startsWith("/mini") || url.searchParams.get("miniApp") === "true"
+
+    if (isMini) {
+      import("@farcaster/miniapp-sdk").then(({ sdk }) => {
+        sdk.actions.ready()
+      })
     }
-    initialize()
   }, [])
 
   return null
